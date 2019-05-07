dbeedcc61f0aa26b85dc10448f700e6bc850bb6f
Abel <esung1129@gmail.com>
2019-05-07 23:09:03 +0900

    :lipstick:Price컴포넌트 스타일 및 레이아웃 작업

--- src/pages/wishlist/Price.js
+++ src/pages/wishlist/Price.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './Price.scss';
 
 const Price = () => {
-  return <div> 총액 : xxx원</div>
+  return (
+    <div className="Price">
+      <p className="Price__title">총 선택 상품</p>
+      <div className="Price__container">
+        <div className="Price__row">
+          <p>총 상품 가격</p>
+          <p>10,000 <span>원</span></p>
+        </div>
+        <div className="Price__row">
+          <p>할인</p>
+          <p className="Price__row__discount">10,000 <span>원</span></p>
+        </div>
+        <div className="Price__last-row">
+          <p>총 결제금액</p>
+          <p className="Price__last-row__price">10,000 <span>원</span></p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Price;
