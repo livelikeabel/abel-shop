1f45ba64db2734604e428b6e37017dce4e25e003
Abel <esung1129@gmail.com>
2019-05-07 14:22:51 +0900

    :sparkles:mock-data와 simpleFetch 만들기

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { simpleFetch } from '../../utils';
 
-const Products = () => {
-  return <div>프로덕트 페이지</div>
+class Products extends Component {
+  componentDidMount() {
+    simpleFetch('productItems').then(res => console.log(res));
+    simpleFetch('coupons').then(res => console.log(res));
+  }
+
+  render() {
+    return <div>프로덕트 페이지</div>
+  }
 }
 
 export default Products
