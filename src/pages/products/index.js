92a612b641beca683045ce582a80206bd53606de
Abel <esung1129@gmail.com>
2019-05-08 12:55:33 +0900

    :hammer:Products 컴포넌트 hooks기반 함수형 컴포넌트로 변경

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProductItem from './ProductItem';
 import PageNation from './PageNation';
 import { simpleFetch } from '../../utils';
 import Loading from '../../components/Loading';
 
-// react16의 함수형 컴포넌트로 바꾸기
-class Products extends Component {
-  state = { productItems: [], pageItems: [], currentPage: 1 };
+const Products = () => {
+  const [productItems, setProductItems] = useState([]);
+  const [pageItems, setPageItems] = useState([]);
 
-  componentDidMount() {
-    simpleFetch('productItems').then(productItems => {
-      productItems.sort((a, b) => b.score - a.score)
-      this.setState(() => ({ productItems }));
+  useEffect(() => {
+    simpleFetch('productItems').then(res => {
+      res.sort((a, b) => b.score - a.score)
+      setProductItems(res)
     });
+  }, [])
+
+  const handleChangePage = pageItems => {
+    setPageItems(pageItems)
   }
 
-  handleChangePage = pageItems => {
-    this.setState(() => ({ pageItems }))
-  }
-
-  renderProducts = () => (
-    this.state.pageItems.map(product => <ProductItem key={product.id} {...product} />)
+  const renderProducts = () => (
+    pageItems.map(product => <ProductItem key={product.id} {...product} />)
   )
-
-  render() {
-    const { productItems } = this.state;
-    if (productItems.length === 0) return <Loading/>;
-    return (
-      <>
-        {this.renderProducts()}
-        <PageNation
-          items={productItems}
-          trimNumber={5}
-          onChagePage={this.handleChangePage}
-        />
-      </>
-    )
-  }
+  
+  if (productItems.length === 0) return <Loading />;
+  return (
+    <>
+      {renderProducts()}
+      <PageNation
+        items={productItems}
+        trimNumber={5}
+        onChagePage={handleChangePage}
+      />
+    </>
+  )
 }
 
 export default Products
